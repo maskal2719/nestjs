ad7516630eb30cc39b89709f912c725437f23e8c
maskal2719 <maskal2719@gmail.com>
2023-10-08 02:06:24 +0300

    first commit

---
+++ src/roles/user-roles.model.ts
@@ -0,0 +1,18 @@
+import {BelongsToMany, Column, ForeignKey, Model, Table} from "sequelize-typescript";
+import {DataTypes} from "sequelize";
+import {User} from "../users/users.model";
+import {Role} from "./roles.model";
+
+@Table({tableName:'user-roles', createdAt: false, updatedAt: false})
+export class UserRoles extends Model<UserRoles> {
+    @Column({type: DataTypes.INTEGER, unique: true, autoIncrement:true, primaryKey: true})
+    id: number;
+
+    @ForeignKey(() => Role)
+    @Column({type: DataTypes.INTEGER})
+    roleId: number;
+
+    @ForeignKey(() => User)
+    @Column({type: DataTypes.INTEGER})
+    userId: number;
+}
---
+++ src/users/users.model.ts
@@ -0,0 +1,22 @@
+import {BelongsToMany, Column, Model, Table} from "sequelize-typescript";
+import {DataTypes} from "sequelize";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
+
+interface UserCreationAttrs {
+    email: string;
+    password: string;
+}
+@Table({tableName:'users'})
+export class User extends Model<User,UserCreationAttrs> {
+    @Column({type: DataTypes.INTEGER, unique: true, autoIncrement:true, primaryKey: true})
+    id: number;
+    @Column({type: DataTypes.STRING, unique: true, allowNull: false})
+    email: string;
+    @Column({type: DataTypes.STRING, allowNull: false})
+    password: string;
+
+
+    @BelongsToMany(() => Role, ()=> UserRoles)
+    roles: Role[]
+}
