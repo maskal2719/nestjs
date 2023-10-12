2358bede64732c802887b79e21fafef0df733f62
maskal2719 <maskal2719@gmail.com>
2023-10-12 11:46:49 +0300

    add menu controller

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as process from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
@@ -9,6 +10,15 @@ async function start() {
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
 }
 
 start();
