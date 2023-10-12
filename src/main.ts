d5b0a38633548e154752268de28ce1f68e1e7fa2
maskal2719 <maskal2719@gmail.com>
2023-10-12 12:08:16 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,16 @@ import * as process from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  const corsOptions: CorsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  };
-
-  app.enableCors(corsOptions);
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
