d79148d59479651abed183c5416a69317e84d8d0
maskal2719 <maskal2719@gmail.com>
2023-10-22 19:33:49 +0300

    fix

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
@@ -8,10 +8,5 @@ export class OrdersController {
   @Post()
   create(@Body() orderDto: any) {
     return this.orderService.createOrder(orderDto);
-  }
-
-  @Get(':id')
-  getOrder(@Param('id') id: number) {
-    return this.orderService.getOrder(id);
   }
 }
